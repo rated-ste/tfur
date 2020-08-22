9772b7bd792e686047ba2cb535d3f9f35b5db517
Ste <ratedste@gmail.com>
2020-08-22 14:35:48 +0000

    Spin up basic web server and lock down security group

--- main.tf
+++ main.tf
@@ -3,10 +3,28 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-    ami           = "ami-0cb72d2e599cffbf9"
-    instance_type = "t2.micro"
+    ami                    = "ami-0d857c06968b4f4fb"
+    instance_type          = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.instance.id]
+
+    user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello, World" > index.html
+                nohup busybox httpd -f -p 8080 &
+                EOF
 
     tags = {
-        Name = "base-node"
+        Name = "base_node"
+    }
+}
+
+resource "aws_security_group" "instance" {
+    name = "base_node_sg"
+
+    ingress {
+        from_port   = 8080
+        to_port     = 8080
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
     }
 }
