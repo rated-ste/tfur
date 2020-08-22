39b79773842493eb051768f4b594977935e10c6c
Ste <ratedste@gmail.com>
2020-08-22 16:26:33 +0000

    deploy ASG, ALB infrastructure to replace single EC2 node

--- main.tf
+++ main.tf
@@ -2,19 +2,126 @@ provider "aws" {
     region = "eu-west-3"
 }
 
-resource "aws_instance" "example" {
-    ami                    = "ami-0d857c06968b4f4fb"
-    instance_type          = "t2.micro"
-    vpc_security_group_ids = [aws_security_group.instance.id]
+data "aws_vpc" "default" {
+    default = true
+}
+
+data "aws_subnet_ids" "default" {
+    vpc_id = data.aws_vpc.default.id
+}
+
+variable "server_port" {
+    description = "HTTP TCP port"
+    type        = number
+    default     = 8080
+}
+
+resource "aws_launch_configuration" "example" {
+    image_id        = "ami-0d857c06968b4f4fb"
+    instance_type   = "t2.micro"
+    security_groups = [aws_security_group.instance.id]
 
     user_data = <<-EOF
                 #!/bin/bash
                 echo "Hello, World" > index.html
-                nohup busybox httpd -f -p 8080 &
+                nohup busybox httpd -f -p ${var.server_port} &
                 EOF
 
-    tags = {
-        Name = "base_node"
+    lifecycle {
+        create_before_destroy = true
+    }              
+}
+
+resource "aws_autoscaling_group" "example" {
+    launch_configuration = aws_launch_configuration.example.name
+    vpc_zone_identifier  = data.aws_subnet_ids.default.ids
+
+    target_group_arns = [aws_lb_target_group.asg.arn]
+    health_check_type = "ELB"
+
+    min_size = 2
+    max_size = 4
+
+    tag {
+        key                 = "Name"
+        value               = "asg_base_node"
+        propagate_at_launch = true
+    }
+}
+
+resource "aws_lb" "example" {
+    name               = "alb-asg-base-node"
+    load_balancer_type = "application"
+    subnets            = data.aws_subnet_ids.default.ids
+    security_groups    = [aws_security_group.alb.id]
+}
+
+resource "aws_lb_listener" "http" {
+    load_balancer_arn = aws_lb.example.arn
+    port              = 80
+    protocol          = "HTTP"
+
+    default_action {
+        type = "fixed-response"
+
+        fixed_response {
+            content_type = "text/plain"
+            message_body = "404: Page not found"
+            status_code  = 404
+        }
+    }
+}
+
+resource "aws_lb_listener_rule" "asg" {
+    listener_arn = aws_lb_listener.http.arn
+    priority     = 100
+
+    condition {
+        path_pattern {
+            values = ["*"]
+        }
+    }
+
+    action {
+        type             = "forward"
+        target_group_arn = aws_lb_target_group.asg.arn
+    }
+}
+
+resource "aws_lb_target_group" "asg" {
+    name     = "asg-base-node"
+    port     = var.server_port
+    protocol = "HTTP"
+    vpc_id   = data.aws_vpc.default.id
+
+    health_check {
+        path                = "/"
+        protocol            = "HTTP"
+        matcher             = "200"
+        interval            = 15
+        timeout             = 3
+        healthy_threshold   = 2
+        unhealthy_threshold = 2
+    }
+}
+
+resource "aws_security_group" "alb" {
+    name = "alb_asg_base_node_sg"
+
+    # Allow inbound HTTTP
+    ingress {
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    # Allow outbound all
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
     }
 }
 
@@ -22,9 +129,14 @@ resource "aws_security_group" "instance" {
     name = "base_node_sg"
 
     ingress {
-        from_port   = 8080
-        to_port     = 8080
+        from_port   = var.server_port
+        to_port     = var.server_port
         protocol    = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
+}
+
+output "alb_dns_name" {
+    value = aws_lb.example.dns_name
+    description = "The public domain name of our ALB"
 }
